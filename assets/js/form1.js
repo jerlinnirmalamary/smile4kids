d993c94fcab9a79d2233f9f9013dff7ab83f641d
unknown <jerlin26399@gmail.com>
2023-10-30 18:10:43 +0530

    added index page

---
+++ assets/js/form1.js
@@ -0,0 +1,33 @@
+//
+// $(function () {
+//   $("#datepicker").datepicker();
+// })(
+//
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+//   function () {
+//     "use strict";
+
+//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//     var forms = document.querySelectorAll(".needs-validation");
+
+//     // Loop over them and prevent submission
+//     Array.prototype.slice.call(forms).forEach(function (form) {
+//       form.addEventListener(
+//         "submit",
+//         function (event) {
+//           if (!form.checkValidity()) {
+//             event.preventDefault();
+//             event.stopPropagation();
+//           }
+
+//           form.classList.add("was-validated");
+//         },
+//         false
+//       );
+//     });
+//   }
+// )();
+
+// c:\Users\Welcome\AppData\Local\Temp\Rar$DRa22384.14724\jquery-ui-1.13.2.custom\jquery-ui.css
+// c:\Users\Welcome\AppData\Local\Temp\Rar$DRa22384.16926\jquery-ui-1.13.2.custom\jquery-ui.js
